fb82e736b56b6a9adcee265f47534a46b4ac8d63
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-02-16 17:13:45 -0500

    commit tree 5febf838ac525f7aade929c6ec12b05b0ea10498

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
   get '/' do
-   puts "Hello, World!" 
-    resp.status = 200
+   "Hello, World!" 
+    
   end 
   
   
