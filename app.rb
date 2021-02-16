b826889cb3802d13e05fe1ad883c26312e0ea7d9
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-02-16 16:54:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+    resp.write "Hello, World!" 
+    resp.status = 200
+  end 
+  
+  
 
 end
